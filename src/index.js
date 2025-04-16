7bd07a813922c0f8023f38b485b68bf7b74fcb88
yuki <38411009+yukijina@users.noreply.github.com>
2025-04-16 07:42:33 -0700

    add hidden and blink class, typewrite and axios

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-let apiKey = '2f5896dd4cc0cdo340203tba4fba205f';
-let myPrompt = '';
-let myContext = '';
-let url = `https://api.shecodes.io/ai/v1/generate?prompt=${myPrompt}&context=${myContext}&key=${apiKey}`;
-
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
+  console.log(response.data.answer);
 
   new Typewriter('.poem-container', {
-    strings: (poemElement.innerHTML = 'can you see the poem result here'),
+    strings: response.data.answer,
     autoStart: true,
-    delay: 50,
+    delay: 60,
     cursor: '',
   });
+}
+
+function generatePoem(event) {
+  console.log('cliced');
+  event.preventDefault();
+  let userInput = document.querySelector('.user-input').value;
+
+  let apiKey = '2f5896dd4cc0cdo340203tba4fba205f';
+  let myContext = 'You are a poem creater. Also you are familier with HTML.';
+  let myPrompt = `Generate a short English poem related to ${userInput}. Poem should be within 10 sentense. Generate a well-organized poem with HTML element such as <h2>, <p>, <br /> <strong>. Omit your answe and html quote`;
+  let url = `https://api.shecodes.io/ai/v1/generate?prompt=${myPrompt}&context=${myContext}&key=${apiKey}`;
+
+  let poemContainer = document.querySelector('.poem-container');
+  poemContainer.classList.remove('hidden');
+  poemContainer.innerHTML = '<p class="blink">Generating a poem...</p>';
+
+  axios.get(url).then(displayPoem);
 }
 let formElement = document.querySelector('.input-form');
 
