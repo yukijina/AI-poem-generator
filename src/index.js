2c6ec7c57e7ddc885db27fb77dafa14bd45afee6
yuki <38411009+yukijina@users.noreply.github.com>
2025-04-17 06:29:51 -0700

    cleam the codes add coments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 function displayPoem(response) {
-  console.log(response.data.answer);
-
+  //  display API response data using Typewriter plug in
   new Typewriter('.poem-container', {
     strings: response.data.answer,
     autoStart: true,
@@ -10,13 +9,13 @@ function displayPoem(response) {
 }
 
 function generatePoem(event) {
-  console.log('cliced');
+  // geenerate poem wth AI API
   event.preventDefault();
   let userInput = document.querySelector('.user-input').value;
 
   let apiKey = '2f5896dd4cc0cdo340203tba4fba205f';
   let myContext = 'You are a poem creater. Also you are familier with HTML.';
-  let myPrompt = `Generate a short English poem related to ${userInput}. Poem should be within 10 sentense. Generate a well-organized poem with HTML element such as <h2>, <p>, <br /> <strong>. Omit your answe and html quote`;
+  let myPrompt = `Generate a short English poem related to ${userInput}. Poem should be within 10 sentense. Generate a well-organized poem with HTML element such as <h2>, <p>, <br /> <strong>. Omit your answe and html quote. At the end of poem, add <strong class="shecode">By SheCodes AI</strong>`;
   let url = `https://api.shecodes.io/ai/v1/generate?prompt=${myPrompt}&context=${myContext}&key=${apiKey}`;
 
   let poemContainer = document.querySelector('.poem-container');
@@ -25,6 +24,7 @@ function generatePoem(event) {
 
   axios.get(url).then(displayPoem);
 }
-let formElement = document.querySelector('.input-form');
 
+// form submit event
+let formElement = document.querySelector('.input-form');
 formElement.addEventListener('submit', generatePoem);
